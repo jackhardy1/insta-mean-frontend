41fc2ca73bb21f8a961a8aba78802a598c89a114
Jack Hardy <jackhardy1@gmail.com>
2016-09-05 22:56:56 +0100

    updates tests

--- test/unit/InstaController.spec.js
+++ test/unit/InstaController.spec.js
@@ -13,4 +13,16 @@ describe('InstaController', function() {
       expect(ctrl.instas[0]).toEqual(objs);
   });
 
+  it('adds a new insta', function() {
+    ctrl.addInsta('NewInsta');
+    var insta = {text: "NewInsta", completed: false};
+    expect(ctrl.instas.pop()).toEqual(insta);
+  });
+
+  it('removes the last insta', function() {
+    var initialCount = ctrl.instas.length;
+    ctrl.removeInsta();
+    expect(ctrl.instas.length).toEqual(initialCount - 1);
+  });
+
 });
